60fbead2731aaa57e4e0fea293cf8be4da4d0d51
Kári Þrastarson <karithra@islandsbanki.is>
2024-04-09 21:26:14 +0000

    updates. Work in progress

---
+++ public/js/apt-details.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+  var aptId = getUrlParameter('apt');
+  
+  $.getJSON("data/data.json", function(data){
+    var found = false;
+    for (var i = 0, len = data.length; i < len; i++) {
+        var value = data[i];
+        if(value.id === aptId) {
+            /* Data point found */
+            found = true;
+            var seld = (String(value.seld).toLowerCase() === "já");
+            $("#apt-info-floor-plan").attr('src', 'media/floor-plans/' + aptId+'.jpg');
+            $("#apt-nr").text("Íbúð " + value.id + (seld ? " (SELD)":(value.ferli != null ? " (í söluferli)" : "")));
+            $("#apt-floor").text(value.haed);
+            $("#apt-size").text(value.birtflatarmal);
+            $("#apt-storage").text(value.geymsla);
+            $("#apt-parking").text(value.bilastaedi);
+            $('#contact-info').click(function(){
+              location.href = "mailto:fyrirspurnir@vesturvik.is?subject=Fyrirspurn um íbúð " + value.id;
+            });
+            $("#apt-price").text(seld ? "Seld" : (value.ferli != null ? "Í söluferli" : value.verd));
+            break;
+        }
+    }
+    if(found === false) {
+        // $(".wrapper-item").hide();
+        // $("#notfound").show();
+    }
+
+}).fail(function(){
+    console.log("An error has occurred.");
+    // $(".wrapper-item").hide();
+    // $("#notfound").show();
+});
+});
+
+
+var getUrlParameter = function getUrlParameter(sParam) {
+  var sPageURL = window.location.search.substring(1),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
+
+      if (sParameterName[0] === sParam) {
+          return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+      }
+  }
+  return false;
+};
--- public/js/main.js
+++ public/js/main.js
@@ -4,13 +4,13 @@ $( document ).ready(function() {
   $('body').on('scroll', function(event) {
     //Change background of nav bar when scrolled
      if ($(this).scrollTop() > 50) {
-         $('#scroll').css('background', 'var(--main)');
+         $('#scroll').css('background', 'transparent');
          $('.topnav a').animate({
  
       }, 500);
      }
      else {
-     $('#scroll').css('background', 'linear-gradient(to bottom, var(--main), transparent)');
+     $('#scroll').css('background', 'var(--secondary)');
      $('.topnav a').animate({
 
   }, 500);
@@ -37,7 +37,7 @@ function populateGallery() {
     var aptGallery = "";
     for (var i = 0, len = data.length; i < len; i++) {
       var value = data[i];
-      var details ='<p><i class="fa fa-bed"></i>' + value.herbergi + ' herbergi</p><p><i class="fas fa-ruler-combined"></i>' + value.birtflatarmal + ' fermetrar</p><p><i class="fas fa-parking"></i>' + value.bilastaedi + ' bílastæði</p><p><i class="fas fa-coins"></i>' + value.verd + ' m.kr</p><button class="card-button">Nánar</button>'
+      var details ='<p><i class="fa fa-bed"></i>' + value.herbergi + ' herbergi</p><p><i class="fas fa-ruler-combined"></i>' + value.birtflatarmal + ' fermetrar</p><p><i class="fas fa-parking"></i>' + value.bilastaedi + ' bílastæði</p><p><i class="fas fa-coins"></i>' + value.verd + ' m.kr</p><a href="apt.html?apt=' + value.ibudnr + '" class="card-button">Nánar</a>'
       var detailsContainer = $('#apt-'+value.ibudnr+' > div');
       detailsContainer.html(details);
       var detailsContainer = $('#apt-'+value.ibudnr).attr('data-stada', value.stada);
@@ -46,7 +46,8 @@ function populateGallery() {
     $('area').click(function(e) {
       var ibudNr = e.target.alt;
       var card = $('#apt-'+ibudNr);
-       var itemPosition = card.offset().left;
+       //var itemPosition = card.offset().left;
+       var itemPosition = card[0].getBoundingClientRect().left + window.scrollX;
        var scrollPosition = itemPosition - ($(window).width() / 2) + (card.outerWidth() / 2);
 
       $('.apt-details').removeClass('selected');
@@ -71,13 +72,13 @@ function populateGallery() {
 
 function burgerCLick(x) {
   x.classList.toggle("change");
-  $('.topnav').toggleClass("show")
+  if ($('.topnav').hasClass("show")) {
+    $('.topnav').removeClass("show").hide();
+  } else {
+    $('.topnav').addClass("show").hide().fadeIn(250);
+  }
 }
 
-function toggleShow(x) {
-    $('.topnav').toggleClass("show")
-    $('.burger').toggleClass("change");
-}
 
 function toggleFullscreen(x) {
     //If x has src attribute, set the src of the fullscreenview to x.src
